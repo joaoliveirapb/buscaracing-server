27305ef580575e05e1221b3f13ccaef8194fe67d
joaoliveirapb <joaoliveira.batista1@gmail.com>
2025-02-17 21:22:34 -0300

    feat: implements findById and delete methods for MotosRepository

--- src/repositories/motos.repository.ts
+++ src/repositories/motos.repository.ts
@@ -2,5 +2,7 @@ import type { Image, Moto, Prisma } from '@prisma/client'
 
 export interface MotosRepository {
   getAll(): Promise<(Moto & { images: Image[] })[]>
+  findById(id: string): Promise<(Moto & { images: Image[] }) | null>
   create(data: Prisma.MotoCreateInput): Promise<Moto>
+  delete(id: string): Promise<void>
 }
